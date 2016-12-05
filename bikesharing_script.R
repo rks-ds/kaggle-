75632cbdce3c172b974fc0b3453231e52b488d9b
Roopkishor singh <roopkishor.singh@rediffmail.com>
2016-12-06 00:37:20 +0500

    third

--- bikesharing_script.R
+++ bikesharing_script.R
@@ -1,3 +1,17 @@
+Bike sharing: Predicting number of bike rented
+Kaggle Problem
+
+In this competition, participants are asked to combine historical usage patterns with weather data in order to
+forecast bike rental demand in the Capital Bikeshare program in Washington, D.C.
+
+You are provided hourly rental data spanning two years. For this competition, the training set is comprised of the
+first 19 days of each month, while the test set is the 20th to the end of the month. You must predict the total count
+of bikes rented during each hour covered by the test set, using only information available prior to the rental period.
+
+This is the simple model for bike sharing which give RMSLE as 0.47 on leaderboard
+
+The approach is simple getting the hour, weekend and month from datetime provided and applying randomforest.
+
 #reading training and test data
 train=read.csv("train.csv")
 test=read.csv("test.csv")
